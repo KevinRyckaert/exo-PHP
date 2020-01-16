8dceb16591dc32256b89bca6c65b234924d34725
KevinRyckaert <kevinryckaert90@gmail.com>
2020-01-16 16:51:55 +0100

    fin de journée

---
+++ excuse.php
@@ -0,0 +1,31 @@
+<!--
+    PHP Exercice
+
+    Dans un fichier "excuse.php", 
+    
+    crée un "générateur d'excuses" pour faciliter la vie des parents d'enfants en école primaire.
+
+    Il te faut un formulaire avec trois questions:
+
+        nom de l'enfant
+        nom de l'institutrice
+        une raison de l'absence à choisir parmi des options (input de type "radio"):
+            maladie
+            décès de l'animal de compagnie (ou d'un membre de la famille)
+            activité extra-scolaire importante
+            toute autre excuse de ton choix
+
+    Pour chaque option, invente une excuse adaptée en fonction de l'option choisie. 
+    
+    Utilise une condition pour afficher la bonne excuse.
+
+    Génère le mot d'excuse utilisant les trois réponses au formulaire.
+
+    Le parent n'aura plus qu'à copier-coller la réponse pour rédiger sa lettre d'excuses.
+    Critères de perfectionnement
+
+        ajoute la date du jour (format "Day, le dd Month YYYY")
+        ajoute la formule de politesse.
+        pas de faute d'orthographe.
+        UX soignée: en faire un vrai produit? ( CSS + typographie + logo )
+-->
